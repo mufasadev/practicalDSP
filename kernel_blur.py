94c7137ff52e61e353be72751019111e7da1eb1e
Dmitry Aveltsov <dmitry.aveltsov@gmail.com>
2018-09-27 01:54:46 +0600

    report added

--- kernel_blur.py
+++ kernel_blur.py
@@ -33,10 +33,24 @@ kernel_stamping1 = np.array([[0, -1, 0],
 kernel_stamping2 = np.array([[0, 1, 0],
                              [-1, 0, 1],
                              [0, -1, 0]])
+kernel_stamping_test = np.array([
+    [0, -1, 0],
+    [0, -1, 0],
+    [1, -1, -1],
+    [1, -1, -1],
+    [1, 0, -1],
+    [1, 1, -1],
+    [1, 1, -1],
+    [0, 1, 0],
+    [0, 1, 0]]) * -1
 
 no_result = np.array([[0, 0, 0],
                       [0, 1, 0],
                       [0, 0, 0]])
+no_result_1 = np.array([[1., 1., 1., 1.],
+                        [1., 1., 1., 1.],
+                        [1., 1., 1., 1.],
+                        [1., 1., 1., 1.]]) / 12
 
 kernel_gaussian = np.array([[1, 4, 6, 4, 1],
                             [4, 16, 24, 16, 4],
@@ -47,7 +61,7 @@ kernel_gaussian = np.array([[1, 4, 6, 4, 1],
 # n = int(input('matrix size (an odd number): '))
 # matrix = np.array([[1.] * n] * n) / (n * n)
 # print matrix
-img = imread('images/test_1_bw.png')
+img = imread('images/a_h_1.png')
 
 imshow(img)
 show()
@@ -55,31 +69,34 @@ show()
 
 # imshow(procImageUsingKernel(img, matrix))
 # show()
-# imshow(cv2.filter2D(img, -1, matrix))
+# imshow(cv2.filter2D(img, -1, no_result_1))
 # show()
 #
 # imshow(procImageUsingKernel(img, kernel_blur))
 # show()
 #
-imshow(procImageUsingKernel(img, no_result))
-show()
+# imshow(procImageUsingKernel(img, no_result))
+# show()
 #
 # imshow(procImageUsingKernel(img, kernel_sharpen1))
 # show()
-#
+# #
 # imshow(procImageUsingKernel(img, kernel_sharpen2))
 # show()
 #
-# imshow(procImageUsingKernel(rgb2gray(img), kernel_edge_detect1))
+# imshow(procImageUsingKernel(img, kernel_edge_detect1))
 # show()
 #
-# imshow(procImageUsingKernel(rgb2gray(img), kernel_edge_detect2))
+# imshow(procImageUsingKernel(img, kernel_edge_detect2))
 # show()
 
 # imshow(procImageUsingKernel(rgb2gray(img), kernel_stamping1, 127))
 # show()
 #
 # imshow(procImageUsingKernel(rgb2gray(img), kernel_stamping2, 127))
+# show()
+#
+# imshow(procImageUsingKernel(rgb2gray(img), kernel_stamping_test, 127))
 # show()
 
 # watercolor
@@ -94,6 +111,6 @@ show()
 
 # imshow(procImageUsingKernel(rgb2gray(img), filters.filter2()))
 # show()
-
+#
 # imshow(median(rgb2gray(img)))
 # show()
